90ff9800b521cd0289f0d565b4b1d6c7b2a2ab86
Alex Rudnick <alex.rudnick@gmail.com>
2012-07-17 23:41:00 -0700

    Some code cleanup; also reformat with google style guide, because Eclipse default is kind of crazy.

--- src/maltlibweka/TimblClassifier.java
+++ src/maltlibweka/TimblClassifier.java
@@ -1,11 +1,9 @@
 package maltlibweka;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ConnectException;
 import java.net.SocketException;
@@ -17,129 +15,106 @@ import weka.core.Instance;
 import weka.core.Instances;
 
 public class TimblClassifier extends Classifier {
+  private static final long serialVersionUID = -1822137392036950026L;
+  private TelnetClient telnetClient;
 
-	private static final long serialVersionUID = -1822137392036950026L;
-	private TelnetClient telnetClient;
+  public TimblClassifier() {
+    telnetClient = null;
+  }
 
-	@Override
-	public void buildClassifier(Instances data) throws Exception {
-		int numAttributes = data.numAttributes();
-		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
-				new FileOutputStream("/tmp/thedataset"), "UTF-8"));
+  @Override
+  public void buildClassifier(Instances data) throws Exception {
+    int numAttributes = data.numAttributes();
+    BufferedWriter out =
+        new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/tmp/thedataset"), "UTF-8"));
 
-		for (int i = 0; i < data.numInstances(); i++) {
-			StringBuffer row = new StringBuffer();
-			Instance inst = data.instance(i);
-			for (int attnum = 0; attnum < numAttributes - 1; attnum++) {
-				row.append("f" + inst.value(attnum) + ",");
-			}
-			row.append("c" + inst.classValue() + "\n");
-			out.write(row.toString());
-		}
-		out.close();
-	}
+    for (int i = 0; i < data.numInstances(); i++) {
+      StringBuffer row = new StringBuffer();
+      Instance inst = data.instance(i);
+      for (int attnum = 0; attnum < numAttributes - 1; attnum++) {
+        row.append("f" + inst.value(attnum) + ",");
+      }
+      row.append("c" + inst.classValue() + "\n");
+      out.write(row.toString());
+    }
+    out.close();
+  }
 
-	public TimblClassifier() {
-		System.out.println("I GOT CONSTRUCTED!!");
-		serverPort = -1;
-		telnetClient = null;
-	}
+  @Override
+  public double classifyInstance(Instance instance) throws Exception {
+    if (telnetClient == null) {
+      initializeConnection();
+    }
 
-	private void initializeConnection() throws SocketException, IOException {
-		telnetClient = new TelnetClient();
-		for (int attempt = 0; attempt < 10; attempt++) {
-			try {
-				telnetClient.connect("localhost", 7000);
-				// skip welcome.
-				InputStream in = telnetClient.getInputStream();
-				while (true) {
-					int c = in.read();
-					if (c == -1 || (char) c == '\n') {
-						break;
-					}
-				}
-			} catch (ConnectException e) {
-				continue;
-			}
-			break;
-		}
-	}
+    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(telnetClient.getOutputStream()));
+    InputStream in = telnetClient.getInputStream();
 
-	@Override
-	protected void finalize() throws Throwable {
-		// TODO Auto-generated method stub
-		super.finalize();
+    StringBuffer toSend = new StringBuffer();
+    toSend.append("c ");
+    for (int attnum = 0; attnum < instance.numAttributes() - 1; attnum++) {
+      toSend.append("f" + instance.value(attnum) + ",");
+    }
+    toSend.append("?\n");
 
-		if (telnetClient != null) {
-			try {
-				BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
-						telnetClient.getOutputStream()));
-				out.write("exit\n");
-				out.flush();
-			} finally {
-				telnetClient.disconnect();
-			}
-		}
-	}
+    String thereply = "";
+    for (int tries = 0; !thereply.startsWith("CATEGORY {c") && tries < 10; tries++) {
+      thereply = getResponseFor(toSend.toString(), out, in);
+    }
+    // We expect the reply to look like:
+    // CATEGORY {cNUMBERGOESHERE}.
+    String justthenumber = thereply.substring(thereply.indexOf('{') + 2, thereply.indexOf('}'));
+    return Double.parseDouble(justthenumber);
+  }
 
-	@Override
-	public double classifyInstance(Instance instance) throws Exception {
-		System.out.println("CLASSIFY INSTANCE");
-		if (serverPort == -1) {
-			// have to spin up the server.
-		}
+  @Override
+  protected void finalize() throws Throwable {
+    super.finalize();
 
-		if (telnetClient == null) {
-			initializeConnection();
-		}
+    if (telnetClient != null) {
+      try {
+        BufferedWriter out =
+            new BufferedWriter(new OutputStreamWriter(telnetClient.getOutputStream()));
+        out.write("exit\n");
+        out.flush();
+      } finally {
+        telnetClient.disconnect();
+      }
+    }
+  }
 
-		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
-				telnetClient.getOutputStream()));
-		InputStream in = telnetClient.getInputStream();
+  private String getResponseFor(String query, BufferedWriter out, InputStream in)
+      throws IOException {
+    StringBuffer reply = new StringBuffer();
+    out.write(query);
+    out.flush();
+    while (true) {
+      int c = in.read();
+      if (c == -1 || (char) c == '\n') {
+        break;
+      }
+      reply.append((char) c);
+    }
+    String thereply = reply.toString();
+    return thereply;
+  }
 
-		StringBuffer toSend = new StringBuffer();
-		toSend.append("c ");
-		for (int attnum = 0; attnum < instance.numAttributes() - 1; attnum++) {
-			toSend.append("f" + instance.value(attnum) + ",");
-		}
-		toSend.append("?\n");
-
-		String thereply = "";
-
-		for (int tries = 0; !thereply.startsWith("CATEGORY {c") && tries < 10; tries++) {
-			System.out.println("loop: " + tries);
-			thereply = getResponseFor(toSend.toString(), out, in);
-		}
-
-		System.out.println("THE REPLY: " + thereply);
-
-		// XXX we might sometimes drop bytes somehow and this is worrisome.
-		// We expect the reply to look like:
-		// CATEGORY {cNUMBERGOESHERE} , but sometimes some letters in CATEGORY
-		// are missing somehow.
-		String justthenumber = thereply.substring(thereply.indexOf('{') + 2,
-				thereply.indexOf('}'));
-		System.out.println("just the number: " + justthenumber);
-		return Double.parseDouble(justthenumber);
-	}
-
-	private String getResponseFor(String query, BufferedWriter out,
-			InputStream in) throws IOException {
-		StringBuffer reply = new StringBuffer();
-		out.write(query);
-		System.out.println("classify this one: " + query);
-		out.flush();
-		while (true) {
-			int c = in.read();
-			if (c == -1 || (char) c == '\n') {
-				break;
-			}
-			reply.append((char) c);
-		}
-		String thereply = reply.toString();
-		return thereply;
-	}
-
-	private int serverPort;
-	// private HashMap<String, Double> classToWekaClass;
+  private void initializeConnection() throws SocketException, IOException {
+    telnetClient = new TelnetClient();
+    for (int attempt = 0; attempt < 10; attempt++) {
+      try {
+        telnetClient.connect("localhost", 7000);
+        // skip welcome.
+        InputStream in = telnetClient.getInputStream();
+        while (true) {
+          int c = in.read();
+          if (c == -1 || (char) c == '\n') {
+            break;
+          }
+        }
+      } catch (ConnectException e) {
+        continue;
+      }
+      break;
+    }
+  }
 }
